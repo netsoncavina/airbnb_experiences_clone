a88b183e18a910cc273eac61fb779b35c2f64220
Netson <netsoncavina@gmail.com>
2022-01-13 01:19:25 -0300

    Card code refactored to be reusable

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,37 +1,27 @@
 /*
-Challenge: Build the Card component
-For now, hard-code in the data (like 
-the rating, title, price, etc.)
-
-Notes:
-- Only render 1 instance (I already did this for you)
-- The star icon and photo (katie-zaferes.png) are in the images 
-  folder for your use
-- Make sure to include:
-    - image
-    - star icon (star.png), rating, and review count
-    - title
-    - cost/person
-- The main purpose of this challenge is to show you where our limitations
-  currently are, so don't worry about the fact that you're hard-coding all
-  this data into the component.
+- img ("katie-zaferes.png")
+- rating ("5.0")
+- reviewCount (6)
+- country (Whatever you want)
+- title ("Life Lessons with Katie Zaferes")
+- price (136)
 */
-import Katie from "../images/katie-zaferes.png";
+// import Katie from "../images/katie-zaferes.png";
 import Star from "../images/star.png";
 export default function Card(props) {
   return (
     <>
       <div className="card">
-        <img src={Katie} className="card--image"></img>
+        <img src={`./images/${props.img}`} className="card--image"></img>
         <div className="card--stats">
           <img className="card--star" src={Star}></img>
-          <span>5.0</span>
-          <span className="gray">(6) • </span>
-          <span className="gray">USA</span>
+          <span>{props.rating}</span>
+          <span className="gray">({props.reviewCount}) • </span>
+          <span className="gray">{props.country}</span>
         </div>
-        <p>Life lessons with Katie Zaferes</p>
+        <p>{props.title}</p>
         <p>
-          <span className="bold">From $136 </span> / person
+          <span className="bold">From ${props.price} </span> / person
         </p>
       </div>
     </>
